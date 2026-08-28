bb1fc144fbcccd9dbe4893fbc5799e9aec2ae8a7
orca <anonimzx7@gmail.com>
2026-08-28 15:57:08 +0700

    v 3.3.0 build 33

--- lib/config/app_version.dart
+++ lib/config/app_version.dart
@@ -5,6 +5,6 @@
 ///   1.1.0 → 2
 ///   2.0.0 → 3
 class AppVersion {
-  static const int code = 32;
-  static const String name = '3.2.0';
+  static const int code = 33;
+  static const String name = '3.3.0';
 }
